2a3651ec980db52d6eaeabf253fa8bd219f08e68
Natalie <ncha0086@student.monash.edu>
2025-05-01 09:40:42 +1000

    got rid of everything

--- user.js
+++
@@ -1,8 +0,0 @@
-class User{
-    constructor(username, password_hash, salt){
-        this.username = username;
-        this.salt = salt;
-        this.password_hash = password_hash;
-    }
-}
-module.exports = User;
