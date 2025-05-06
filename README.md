90e961cb0fbaec8d1e1de382ea92de2130726f37
Natalie <ncha0086@student.monash.edu>
2025-05-06 23:21:36 +1000

    added in blind signature to code. replaced part of /vote function to include signature scheme, more secure.

---
+++ README.md
@@ -0,0 +1,23 @@
+# FIT5163
+Requirements:
+    Secure voting system. 
+    Develop a secure online voting system that ensures the integrity, confidentiality, and verifiability of votes. 
+
+    a. Implement a secure voting protocol that uses cryptographic techniques to ensure vote confidentiality and integrity. 
+
+    b. Develop a user interface for voter registration, voting, and result viewing. 
+
+    c. Ensure voter authentication and authorization, preventing double voting and unauthorised access. 
+
+    d. Implement a mechanism for voters to verify that their vote was counted without revealing their choice. (optional, will give 2 bonus marks) 
+
+    e. Use blockchain technology to provide an immutable audit trail of votes (optional, will give 2 bonus marks). 
+
+Parties:
+- voter
+- Election authority - signer
+- Tallying authority
+
+INSTRUCTIONS 
+In terminal, execute the app.py file and from the terminal either press the link or type in 'http://localhost:8000'
+
--- app.py
+++ app.py
@@ -6,12 +6,117 @@ import time
 import random
 import json
 from datetime import datetime
-
-app = Flask(__name__)
-app.secret_key = 'secure-voting-secret-key'
-
 BLOCKCHAIN_FILE = 'blockchain.json'
 
+import cryptography
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes, serialization
+### testing blind signatures
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes 
+import Crypto.Random 
+import math
+
+app = Flask(__name__)
+
+app.secret_key = 'secure-voting-secret-key' # this is only session security, not group signature
+
+#rsa based keys for group signature
+group_master_key = private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+)
+group_public_key = group_master_key.public_key
+
+#RSA for blind signatures - signer pair
+private_key  = RSA.generate(2048)
+public_key = private_key.publickey()
+d = private_key.d
+e = private_key.e
+n = private_key.n
+
+## Key related functions for group signature - bad
+"""
+def generate_user_key(username): # generates new key for given user
+    info = username.encode() #encode username to bytes
+    salt = os.urandom(16)
+    #initialise key derivation object
+    hkdf = HKDF(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    info=info,
+    )  
+    #create new key from master key
+    #convert master key into bytes
+    private_bytes = group_master_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.TraditionalOpenSSL,
+    encryption_algorithm=serialization.NoEncryption()
+    )
+    #derive key
+    user_key = hkdf.derive(private_bytes)
+    return user_key, salt, info
+
+def sign_vote(message_hash, key):
+    signature = key.sign(
+        message_hash, 
+        padding,padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+        )
+    return signature
+ 
+def verify_signature(signature, message):
+    return group_public_key.verify(
+    signature,
+    message,
+    padding.PSS(
+        mgf=padding.MGF1(hashes.SHA256()),
+        salt_length=padding.PSS.MAX_LENGTH
+    ),
+    hashes.SHA256()
+)
+
+key = generate_user_key("test")
+#signature = sign_vote("test1", key)
+print(key[0])
+#print(verify_signature(signature,"test1" ))    
+"""
+### Blind signature
+def prepare_message(msg_byte): # add more randomness into, since voting is 2 options
+    nonce = get_random_bytes(16) 
+    message = nonce+msg_byte
+    hashed = hashlib.sha256(message).digest()
+    hashed_int = int.from_bytes(hashed, byteorder='big') % n
+    return hashed_int, nonce
+
+def blind_message(msg):
+        r = random.randint(2,n-1)
+        while math.gcd(r,n) != 1:
+            r = random.randint(2,n-1)
+        r_e = pow(r,e,n)
+        blinded = (msg*r_e) % n
+        return blinded
+
+def sign_blind(msg):
+    return pow(msg,d, n)
+
+def verify(sig,vote, nonce):
+    digest = hashlib.sha256(nonce+vote).digest()
+    sig_digest = sig**e%n 
+    return sig_digest == digest
+
+def unblind(sig, r):
+    r_1 = pow(r,-1,n)
+    unb_sig = sig * (r_1)%n
+    return unb_sig
+
+#securely recording vote hashes
 class Blockchain:
     def __init__(self):
         self.chain = []
@@ -53,7 +158,11 @@ blockchain = Blockchain()
 def init_db():
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
-
+    # just in case
+    c.execute('''DROP TABLE IF EXISTS users''')
+    c.execute('''DROP TABLE IF EXISTS votes''')
+    c.execute('''DROP TABLE IF EXISTS vote_ledger''')
+    #c.execute('''DROP TABLE IF EXISTS user_key_vault''')
 
     c.execute('''CREATE TABLE IF NOT EXISTS users (
                     username TEXT PRIMARY KEY, 
@@ -68,12 +177,19 @@ def init_db():
     c.execute('''CREATE TABLE IF NOT EXISTS vote_ledger (
                     receipt TEXT PRIMARY KEY,
                     vote_hash TEXT)''')
-
+    
+    #this is a table that stores current group keys
+    """c.execute('''Create table if not exists user_key_vault(
+                username Text primary key,
+                salt blob,
+                info Blob,
+                encrypted_key Text
+              )''')"""
 
     for candidate in ['Alice', 'Bob']:
         c.execute('INSERT OR IGNORE INTO votes (candidate, count) VALUES (?, ?)', (candidate, 0))
 
-
+    #hardcode voting authority user to view votes
     admin_username = 'admin'
     admin_password = hashlib.sha256('adminpass'.encode()).hexdigest()
     c.execute('INSERT OR IGNORE INTO users (username, password, voted, voted_for) VALUES (?, ?, 0, NULL)',
@@ -81,6 +197,7 @@ def init_db():
 
     conn.commit()
     conn.close()
+
 #generate receipt for successful voting submissions
 def generate_receipt(username, candidate):
     nonce = str(time.time()) + str(random.randint(1000, 9999))
@@ -128,16 +245,26 @@ def vote():
         return render_template('error.html', message="You must be logged in to access this page.")
 
     username = session['user']
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    c.execute('SELECT voted FROM users WHERE username=?', (username,))
-    voted = c.fetchone()[0]
-
-    if voted == 1:
-        return redirect('/already_voted')
 
     if request.method == 'POST':
         choice = request.form['candidate']
+        msg, nonce = prepare_message(choice.encode())
+        blinded_msg = blind_message(msg)
+        #check for user
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        c.execute('SELECT voted FROM users WHERE username=?', (username,))
+        voted = c.fetchone()[0]
+        if voted == 1:
+            return redirect('/already_voted')
+        else:
+            #sign vote
+            signature = sign_blind(blinded_msg)
+            timestamp = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+            return render_template('receipt.html', receipt=signature, nonce=nonce, timestamp=timestamp)
+        
+
+        """ previous code has no security
         receipt, vote_hash, nonce = generate_receipt(username, choice)
         c.execute('UPDATE votes SET count = count + 1 WHERE candidate = ?', (choice,))
         c.execute('UPDATE users SET voted = 1, voted_for = ? WHERE username = ?', (choice, username))
@@ -147,6 +274,7 @@ def vote():
         conn.close()
         timestamp = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
         return render_template('receipt.html', receipt=receipt, nonce=nonce, timestamp=timestamp)
+        """
 
     return render_template('vote.html', voted=False)
 
@@ -263,6 +391,12 @@ def result_chart():
     conn.close()
     return render_template('result_chart.html', results=results)
 
+
+@app.route('/verify_vote')
+def verify_vote():
+    signature, nonce = request.form['receipt','nonce']
+    unb_sig = unblind(signature, nonce)
+    return
 
 if __name__ == '__main__':
     init_db()
